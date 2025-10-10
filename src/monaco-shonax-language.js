f2c8b34c2520f9cd64c9731bda42f1d1690841fd
murirop <panashemuriro2021@gmail.com>
2025-10-10 18:22:50 +0200

    initial setup shonax minial working

---
+++ src/monaco-shonax-language.js
@@ -0,0 +1,115 @@
+/* eslint-disable no-useless-escape */
+// monaco-shonax-language.js
+export const ShonaxLanguageDefinition = {
+  defaultToken: '',
+  tokenPostfix: '.shonax',
+  
+  keywords: [
+    'pane', 'kana', 'tarisa', 'zvanyorwa', 'ikanyorwa', 'ikasarudzwa',
+    'zvasarudzwa', 'ronga', 'vakasiyana', 'akasiyana', 'dzakasiyana',
+    'tambira', 'ane', 'vane', 'mune', 'mine', 'une', 'rine', 'ine',
+    'dzine', 'chokwadi', 'kwete', 'uye', 'and', 'or', 'zvimwe',
+    'function', 'basa', 'kuti', 'nyora', 'return', 'dzosa', 'isa',
+    'bvisa', 'mu', 'class', 'verenga', 'batanidza', 'pa', 'bvunza',
+    'tora', 'kubva', 'ku', 'jamba', 'mira', 'dzokorora', 'mirira',
+    'seconds', 'ndi', 'i'
+  ],
+  
+  operators: [
+    '=', '>', '<', '!', '~', '?', ':', '==', '<=', '>=', '!=',
+    '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^',
+    '%', '<<', '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=',
+    '^=', '%=', '<<=', '>>=', '>>>='
+  ],
+  
+  // Regular expressions
+  symbols: /[=><!~?:&|+\-*\/\^%]+/,
+  escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+  
+  tokenizer: {
+    root: [
+      // HTML tags
+      [/<\/?[a-zA-Z][\w\-]*/, 'tag'],
+      
+      // Identifiers and keywords
+      [/[a-zA-Z_]\w*/, {
+        cases: {
+          '@keywords': 'keyword',
+          '@default': 'identifier'
+        }
+      }],
+      
+      // Whitespace
+      { include: '@whitespace' },
+      
+      // Numbers
+      [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+      [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+      [/\d+/, 'number'],
+      
+      // Strings
+      [/"([^"\\]|\\.)*$/, 'string.invalid'],
+      [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+      
+      // Delimiters and operators
+      [/[{}()\\[\]]/, '@brackets'],
+      [/@symbols/, {
+        cases: {
+          '@operators': 'operator',
+          '@default': ''
+        }
+      }],
+      
+      // Shona expressions in HTML
+      [/{/, { token: 'delimiter.bracket', next: '@shonaExpression' }],
+    ],
+    
+    string: [
+      [/[^\\"]+/, 'string'],
+      [/@escapes/, 'string.escape'],
+      [/\\./, 'string.escape.invalid'],
+      [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+    ],
+    
+    shonaExpression: [
+      [/}/, { token: 'delimiter.bracket', next: '@pop' }],
+      { include: 'root' }
+    ],
+    
+    whitespace: [
+      [/[ \t\r\n]+/, 'white'],
+      [/\/\/.*$/, 'comment'],
+      [/\/\*/, 'comment', '@comment']
+    ],
+    
+    comment: [
+      [/[^\/*]+/, 'comment'],
+      [/\/\*/, 'comment', '@push'],
+      ['\\*/', 'comment', '@pop'],
+      [/[\/*]/, 'comment']
+    ]
+  }
+};
+
+export const ShonaxTheme = {
+  base: 'vs-dark',
+  inherit: true,
+  rules: [
+    { token: 'keyword', foreground: 'c586c0' },
+    { token: 'identifier', foreground: '9cdcfe' },
+    { token: 'string', foreground: 'ce9178' },
+    { token: 'number', foreground: 'b5cea8' },
+    { token: 'comment', foreground: '6a9955' },
+    { token: 'tag', foreground: '569cd6' },
+    { token: 'operator', foreground: 'd4d4d4' },
+    { token: 'delimiter.bracket', foreground: 'ffd700' }
+  ],
+  colors: {
+    'editor.background': '#1e1e1e',
+    'editor.foreground': '#d4d4d4',
+    'editor.lineHighlightBackground': '#2a2a2a',
+    'editorCursor.foreground': '#ffffff',
+    'editor.selectionBackground': '#264f78',
+    'editor.inactiveSelectionBackground': '#3a3d41'
+  }
+};
